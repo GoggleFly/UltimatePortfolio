cad51622dd65620be2609d6d82ee14c12cf0f2e2
David Ash <david.ash@ottid.co.uk>
2024-06-17 17:42:23 +0100

    Added ability to select and edit issues

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -11,6 +11,7 @@ class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
     
     @Published var selectedFilter: Filter? = Filter.all
+    @Published var selectedIssue: Issue?
     
     static var preview: DataController {
         let dataController = DataController(inMemory: true)
@@ -100,5 +101,15 @@ class DataController: ObservableObject {
         
         let issueRequest: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
         delete(issueRequest)
+    }
+    
+    func missingTags(from issue: Issue) -> [Tag] {
+        let request = Tag.fetchRequest()
+        let allTags = (try? container.viewContext.fetch(request)) ?? []
+        
+        let allTagsSet = Set(allTags)
+        let difference = allTagsSet.symmetricDifference(issue.issueTags)
+        
+        return difference.sorted()
     }
 }
---
+++ UltimatePortfolio/NoIssueView.swift
@@ -0,0 +1,26 @@
+//
+//  NoIssueView.swift
+//  UltimatePortfolio
+//
+//  Created by David Ash on 17/06/2024.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        Text("No Issue Selected")
+            .font(.title)
+            .foregroundStyle(.secondary)
+        
+        Button("New Issue") {
+            
+        }
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
