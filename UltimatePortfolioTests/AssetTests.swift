64269372e6a95395aafdadbcf06a781ad49548ca
David Ash <david.ash@ottid.co.uk>
2024-07-05 16:18:34 +0100

    Baseline test target setup, with asset tests

---
+++ UltimatePortfolioTests/AssetTests.swift
@@ -0,0 +1,27 @@
+//
+//  AssetTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by David Ash on 05/07/2024.
+//
+
+import XCTest
+@testable import UltimatePortfolio
+
+final class AssetTests: XCTestCase {
+
+    func testColorsExist() {
+        let allColors = ["Dark Blue", "Dark Gray", "Gold", "Gray", "Green",
+                         "Light Blue", "Midnight", "Orange", "Pink", "Purple",
+                         "Red", "Teal"]
+
+        for color in allColors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from asset catalog.")
+        }
+    }
+
+    func testAwardsLoadCorrectly() {
+        XCTAssertTrue(Award.allAwards.isEmpty == false, "Failed to load awards from JSON.")
+    }
+
+}
