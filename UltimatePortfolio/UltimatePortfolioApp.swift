6d9a704a74a6f66c58d57a42493af8eb1dfd1dfc
David Ash <david.ash@ottid.co.uk>
2024-07-13 19:56:52 +0100

    Added basic spotlight integration

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -5,6 +5,7 @@
 //  Created by David Ash on 10/06/2024.
 //
 
+import CoreSpotlight
 import SwiftUI
 
 @main
@@ -29,6 +30,14 @@ struct UltimatePortfolioApp: App {
                     dataController.save()
                 }
             }
+            .onContinueUserActivity(CSSearchableItemActionType, perform: loadSpotlightItem)
+        }
+    }
+
+    func loadSpotlightItem(_ userActivity: NSUserActivity) {
+        if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
+            dataController.selectedIssue = dataController.issue(with: uniqueIdentifier)
+            dataController.selectedFilter = .all
         }
     }
 }
